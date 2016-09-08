38d5e278505027698fcbf8c6fbca927da2af34f1
meduza11 <maximeremenko92@gmail.com>
2016-09-08 13:47:30 +0300

    продукты добавлены

---
+++ resources/views/products.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- Bootstrap шаблон... -->
+<div class="panel-body">
+    <!-- Отображение ошибок проверки ввода -->
+    @include('common.errors')
+    <!-- Форма новой задачи -->
+    <form action="/product" method="POST" class="form-horizontal">
+ {{ csrf_field() }}
+ <!-- Имя задачи -->
+ <div class="form-group">
+     <label for="task" class="col-sm-3 control-label">Задача</label>
+     <div class="col-sm-2">
+                <input type="text" name="name" id="task-name" class="form-control" placeholder="название товара">
+     </div>
+            <div class="col-sm-2">
+                <input type="text" name="price" id="task-name" class="form-control" placeholder="цена">
+     </div>
+            <div class="col-sm-2">
+                <input type="text" name="discription" id="task-name" class="form-control" placeholder="описание">
+     </div>
+            <div class="col-sm-2">
+                <select name="category">
+                    <option>low</option>
+                    <option>medium</option>
+                    <option>hight</option>
+                </select>
+     </div>
+ </div>
+ <!-- Кнопка добавления задачи -->
+ <div class="form-group">
+     <div class="col-sm-offset-3 col-sm-6">
+  <button type="submit" class="btn btn-default">
+      <i class="fa fa-plus"></i> Добавить товар
+  </button>
+     </div>
+ </div>
+    </form>
+</div>
+<!-- Текущие задачи -->
+@if (count($products) > 0)
+<div class="panel panel-default">
+    <div class="panel-heading">
+ Товары
+    </div>
+    <div class="panel-body">
+ <table class="table table-striped task-table">
+     <!-- Заголовок таблицы -->
+     <thead>
+     <th>Название</th>
+     <th>Цена</th>
+            <th>Описание</th>
+            <th>Качество</th>
+            <th>Действие</th>
+     </thead>
+     <!-- Тело таблицы -->
+     <tbody>
+  @foreach ($products as $product)
+  <tr>
+      <!-- Имя задачи -->
+      <td class="table-text">
+   <div>{{ $product->name }}</div>
+      </td>
+                    <td class="table-text">
+   <div>{{ $product->price }}</div>
+      </td>
+                    <td class="table-text">
+   <div>{{ $product->discription }}</div>
+      </td>
+                    <td class="table-text">
+   <div>{{ $product->category }}</div>
+      </td>
+      <!-- Кнопка Удалить -->
+      <td>
+   <form action="/product/{{ $product->id }}" method="POST">
+       {{ csrf_field() }}
+       {{ method_field('DELETE') }}
+       <button>Удалить</button>
+   </form>
+                        <form action="/edit/{{ $product->id }}" method="POST">
+       {{ csrf_field() }}
+       {{ method_field('EDIT') }}
+       <button>Редактировать</button>
+   </form>
+      </td>  
+  </tr>
+  @endforeach
+     </tbody>
+ </table>
+    </div>
+</div>
+@endif
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,62 @@
 <?php
 
-use App\Task;
-use Illuminate\Http\Request;
-
-/**
- * Вывести список всех задач
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-Route::get('/', function () {
-  $tasks = Task::orderBy('created_at', 'asc')->get();
 
-  return view('tasks', [
-    'tasks' => $tasks
+ use App\Task;
+ use App\Product;
+  use Illuminate\Http\Request;
+
+  /**
+   * Вывести список всех задач
+   */
+
+Route::get('/', function () {
+  $products = Product::orderBy('created_at', 'asc')->get();
+  return view('products', [
+    'products' => $products
   ]);
 });
+  /**
+   * Добавить новую задачу
+   */
 
-/**
- * Добавить новую задачу
- */
-Route::post('/task', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-                'name' => 'required|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/')
-                        ->withInput()
-                        ->withErrors($validator);
-    }
-
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-
-    return redirect('/');
+ Route::post('/product', function (Request $request) {
+//  $validator = Validator::make($request->all(), [
+//    'name' => 'required|max:255',
+//  ]);
+//  if ($validator->fails()) {
+//    return redirect('/')
+//      ->withInput()
+//      ->withErrors($validator);
+//  }
+$product = new Product();
+$product->name = $request->name;
+$product->price = $request->price;
+$product->discription = $request->discription;
+$product->category = $request->category;
+$product->save();
+return redirect('/');
+  // Создание задачи...
 });
-/**
- * Удалить существующую задачу
- */
-Route::delete('/task/{id}', function ($id) {
-  Task::findOrFail($id)->delete();
+
+  /**
+   * Удалить существующую задачу
+   */
+
+   Route::delete('/product/{id}', function ($id) {
+  Product::findOrFail($id)->delete();
 
   return redirect('/');
+  
+});
+
+Route::get('/edit/{$id}', function ($id) {
+  $product = Product::findOrFail($id)->get();
+  return view('editProduct', [
+    'product' => $product
+  ]);
 });
